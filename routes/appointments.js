ff9331e99f140db680eefac1b1847bb6253a81e3
IT21262968 <lavinduwelikala@gmail.com>
2025-04-30 15:09:10 +0530

    Read me file added

--- routes/appointments.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { bookAppointment, getAppointments, getMyAppointments, cancelAppointment } = require('../controllers/appointmentController');
-
-router.route('/').get(getAppointments).post(bookAppointment);
-router.route('/my').get(getMyAppointments);
-router.route('/:id/cancel').put(cancelAppointment);
-
-module.exports = router;
--- routes/payments.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { processPayment, getPayments } = require('../controllers/paymentController');
-
-router.route('/').get(getPayments).post(processPayment);
-
-module.exports = router;
